db6222546f38204293ca29598e9727d839d4efe1
HAYASHI Kentaro <kenhys@gmail.com>
2011-02-21 10:29:10 +0900

    import header from ulh3267a and unzp542d
    original bzr status:
    revno: 4
    committer: HAYASHI Kentaro <kenhys@gmail.com>
    branch nick: chkarchpasswd
    timestamp: Mon 2011-02-21 10:29:10 +0900

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
-#gcc -shared -o chkarchpasswd.dll ./.libs/chkarchpasswd.o  ../../libsylph/.libs/libsylph-0.a ../../src/.libs/libsylpheed-plugin-0.a `pkg-config --libs gtk+-2.0` `pkg-config --libs glib-2.0` -L/mingw/local/lib -lssleay32 -leay32 -lws2_32 -liconv -lonig   /mingw/lib/libiconv.a 
 
 TARGET=chkarchpasswd.dll
 OBJS=chkarchpasswd.o
 LIBSYLPH=./lib/libsylph-0-1.a
 LIBSYLPHEED=./lib/libsylpheed-plugin-0-1.a
-#LIBS=" -lglib-2.0-0  -lintl"
-LIBS=" `pkg-config --libs glib-2.0` `pkg-config --libs gobject-2.0` `pkg-config --libs gtk+-2.0`"
 INC=" -I. -I../../ -I../../libsylph -I../../src "
 INC="$INC `pkg-config --cflags glib-2.0 cairo gdk-2.0 atk `"
 DEF=" -DHAVE_CONFIG_H"
 com="gcc -Wall -c $DEF $INC chkarchpasswd.c"
 echo $com
 eval $com
+
+#LIBS=" -lglib-2.0-0  -lintl"
+LIBS=" `pkg-config --libs glib-2.0 gobject-2.0 gtk+-2.0` -L./lib -lunzip32"
 
 com="gcc -shared -o $TARGET $OBJS -L./lib $LIBSYLPH $LIBSYLPHEED $LIBS -lssleay32 -leay32 -lws2_32 -liconv "
 echo $com
