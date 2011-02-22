2783aa0cea59f41c3ddf86e65e048d0a06e70da0
HAYASHI Kentaro <kenhys@gmail.com>
2011-02-22 20:14:15 +0900

    add 7-zip32.h and test code.
    original bzr status:
    revno: 6
    committer: HAYASHI Kentaro <kenhys@gmail.com>
    branch nick: chkarchpasswd
    timestamp: Tue 2011-02-22 20:14:15 +0900

--- build.sh
+++ build.sh
@@ -6,17 +6,28 @@ LIBSYLPHEED=./lib/libsylpheed-plugin-0-1.a
 INC=" -I. -I./include -I../../ -I../../libsylph -I../../src "
 INC="$INC `pkg-config --cflags glib-2.0 cairo gdk-2.0 atk `"
 DEF=" -DHAVE_CONFIG_H -DUNICODE -D_UNICODE"
-com="gcc -Wall -c $DEF $INC chkarchpasswd.c"
-echo $com
-eval $com
-if [ $? != 0  ]; then
-    echo "compile error"
-    exit
-fi
-#LIBS=" -lglib-2.0-0  -lintl"
+
 LIBS=" `pkg-config --libs glib-2.0 gobject-2.0 gtk+-2.0` -L./lib -lunzip32 -lunlha32 ./lib/lib7zip32.a"
 
-com="gcc -shared -o $TARGET $OBJS -L./lib $LIBSYLPH $LIBSYLPHEED $LIBS -lssleay32 -leay32 -lws2_32 -liconv "
-echo $com
-eval $com
+if [ -z "$1" ]; then
 
+    com="gcc -Wall -c $DEF $INC chkarchpasswd.c"
+    echo $com
+    eval $com
+    if [ $? != 0  ]; then
+        echo "compile error"
+        exit
+    fi
+#LIBS=" -lglib-2.0-0  -lintl"
+
+    com="gcc -shared -o $TARGET $OBJS -L./lib $LIBSYLPH $LIBSYLPHEED $LIBS -lssleay32 -leay32 -lws2_32 -liconv "
+    echo $com
+    eval $com
+
+fi
+
+if [ ! -z "$1" ]; then
+    com="gcc -Wall $DEF $INC test7zip32.c -o test7zip32.exe $LIBS"
+    echo $com
+    eval $com
+fi
---
+++ test7zip32.c
@@ -0,0 +1,62 @@
+/*
+ * test7zip32.c
+ * Copyright (C) 2011 HAYASHI Kentaro <kenhys@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include "defs.h"
+
+#include <glib.h>
+#include <gtk/gtk.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#include "sylmain.h"
+#include "plugin.h"
+#include "procmsg.h"
+#include "procmime.h"
+#include "utils.h"
+#include "alertpanel.h"
+#include "compose.h"
+
+#include "unzip32.h"
+
+#include <glib/gi18n-lib.h>
+
+typedef DWORD (CALLBACK* GETVERSIONPROC)(void);
+
+
+int main(int argc, char *argv[])
+{
+  HANDLE hDLL = LoadLibrary(L"7-zip32");
+  DWORD dwVersion;
+  if (hDLL != NULL){
+      GETVERSIONPROC hfunc = (GETVERSIONPROC)GetProcAddress(hDLL, "SevenZipGetVersion");
+      if (!hfunc){
+      }else{
+          dwVersion = hfunc();
+          /* 256*3+16*9+8 = 920 -> 9.20 */
+          DWORD dwHi = dwVersion & 0x0000ffff;
+          g_print("%08lx\n", dwHi);
+          DWORD dwLow = ((dwVersion & 0xffff0000) >>16);
+          g_print("%08lx\n", dwLow);
+      }
+  }
+  return 0;
+}
