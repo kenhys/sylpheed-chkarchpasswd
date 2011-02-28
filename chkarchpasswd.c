061a2e3b7f934395b8ab47e7d4bb66c081c5050e
HAYASHI Kentaro <kenhys@gmail.com>
2011-03-01 03:18:35 +0900

    remove old hook
    original bzr status:
    revno: 16
    committer: HAYASHI Kentaro <kenhys@gmail.com>
    branch nick: chkarchpasswd
    timestamp: Tue 2011-03-01 03:18:35 +0900

--- chkarchpasswd.c
+++ chkarchpasswd.c
@@ -50,6 +50,8 @@ static void exec_chkarchpasswd_cb(GObject *obj, FolderItem *item, const gchar *f
 static void exec_chkarchpasswd_menu_cb(void);
 static void compose_created_cb(GObject *obj, gpointer compose);
 static void compose_destroy_cb(GObject *obj, gpointer compose);
+static gboolean compose_send_cb(GObject *obj, gpointer compose);
+static gboolean compose_sendl_cb(GObject *obj, gpointer compose);
 static void check_attachement_cb(GObject *obj, gpointer compose);
 
 void plugin_load(void)
@@ -66,6 +68,10 @@ void plugin_load(void)
   syl_plugin_signal_connect("compose-created", G_CALLBACK(compose_created_cb), NULL);
 
   syl_plugin_signal_connect("compose-destroy", G_CALLBACK(compose_destroy_cb), NULL);
+
+  syl_plugin_signal_connect("compose-send", G_CALLBACK(compose_send_cb), NULL);
+
+  syl_plugin_signal_connect("compose-sendl", G_CALLBACK(compose_sendl_cb), NULL);
 
   debug_print("[PLUGIN] chkarchpasswd_tool plug-in loading done.\n");
 }
@@ -172,80 +178,6 @@ static gulong g_sendbtn_id = 0;
 
 typedef DWORD (CALLBACK* GETVERSIONPROC)(void);
 
-static gboolean
-button_press_emission_hook (GSignalInvocationHint	*ihint,
-                            guint			n_param_values,
-                            const GValue			*param_values,
-                            gpointer			data)
-{
-
-  GObject *object;
-  GdkEventCrossing *event;
-
-  debug_print("[PLUGIN] button_press_emission_hook called.\n");
-
-  debug_print("[PLUGIN] signal id is %d.\n", ihint->signal_id);
-  debug_print("[PLUGIN] GQuark is %x.\n", ihint->detail);
-  debug_print("[PLUGIN] GSignalFlags %x.\n", ihint->run_type);
-
-
-  object = g_value_get_object (param_values + 0);
-  event  = g_value_get_boxed (param_values + 1);
-
-    
-  debug_print("[PLUGIN] button handler id is %ld.\n", g_sendbtn_id);
-
-  LPSTR szOutput;
-  DWORD dwSize;
-  /* UnZip(NULL, " --i .zip [dir name]", szOutput, dwSize);*/
-  HANDLE hdll = LoadLibrary(L"7-zip32");
-  if (hdll != NULL){
-      GETVERSIONPROC hfunc = (GETVERSIONPROC)GetProcAddress(hdll, "SevenZipGetVersion");
-      if (!hfunc){
-      }else{
-          DWORD dwVersion = hfunc();
-      }
-  }
-#if 0
-  g_signal_stop_emission(G_OBJECT(g_sendbtn), g_signal_id, 0);
-
-  /*gboolean bconnected = g_signal_handler_is_connected(G_OBJECT(g_sendbtn), g_sendbtn_id);
-  debug_print("[PLUGIN] button handler id connected ? %s.\n", bconnected ? "TRUE" : "FALSE");
-  */
-  /*g_signal_handler_block(G_OBJECT(g_sendbtn), g_sendbtn_id);*/
-
-
-  guint blocked = 0;
-  /*g_signal_handlers_block_matched(G_OBJECT(g_sendbtn),
-                                                 G_SIGNAL_MATCH_ID | G_SIGNAL_MATCH_UNBLOCKED,
-                                                 g_signal_id,
-                                                 0,
-                                                 NULL,
-                                                 NULL,
-                                                 NULL);*/
-#else
-  gboolean bconnected = g_signal_handler_is_connected(G_OBJECT(GTK_BIN(g_sendbtn)->child), g_sendbtn_id);
-  debug_print("[PLUGIN] button handler id connected ? %s.\n", bconnected ? "TRUE" : "FALSE");
-  
-  /* g_signal_handler_block(G_OBJECT(GTK_BIN(g_sendbtn)->child), g_sendbtn_id);*/
-
-  guint blocked = g_signal_handlers_block_matched(G_OBJECT(GTK_BIN(g_sendbtn)->child),
-                                                 G_SIGNAL_MATCH_ID | G_SIGNAL_MATCH_UNBLOCKED,
-                                                 g_signal_id,
-                                                 0,
-                                                 NULL,
-                                                 NULL,
-                                                 NULL);
-#endif
-  debug_print("[PLUGIN] number of blocked handler is %d.\n", blocked);
-  
-  /*g_signal_stop_emission(G_OBJECT(GTK_BIN(g_sendbtn)->child), g_signal_id, 0);*/
-  g_signal_stop_emission_by_name(G_OBJECT(GTK_BIN(g_sendbtn)), "button_pressed_event");
-  g_signal_stop_emission_by_name(G_OBJECT(GTK_BIN(g_sendbtn)), "clicked");
-
-  return TRUE;
-}
-
 static void destroy_notify(gpointer data)
 {
   debug_print("[PLUGIN] destroy_notify called.\n");
@@ -255,19 +187,6 @@ void compose_created_cb(GObject *obj, gpointer compose)
 {
   Compose* pComp = (Compose*)compose;
   GtkWidget *toolbar = pComp->toolbar;
-
-  /* get global button-press-eventsignal id */
-#if 1
-  g_signal_id = g_signal_lookup("button-press-event", GTK_TYPE_BUTTON);
-  debug_print("[PLUGIN] signal_id of button_press_event:%d\n", g_signal_id);
-#else
-  g_signal_id = g_signal_lookup("clicked", GTK_TYPE_TOOL_BUTTON);
-  debug_print("[PLUGIN] signal_id of clicked:%d\n", g_signal_id);
-#endif
-  
-  if (g_signal_id == 0){
-    return;
-  }
 
   /* add check archive button for testing. */
   /* GtkWidget *icon = gtk_image_new_from_stock(GTK_STOCK_DIALOG_AUTHENTICATION, GTK_ICON_SIZE_LARGE_TOOLBAR);*/
@@ -282,65 +201,8 @@ void compose_created_cb(GObject *obj, gpointer compose)
   g_signal_connect(G_OBJECT(GTK_BIN(toolitem)->child),
                    "button_press_event",
                    G_CALLBACK(check_attachement_cb), compose);
-  gtk_widget_show(toolbar);
+  gtk_widget_show_all(toolbar);
   
-  g_sendbtn = gtk_toolbar_get_nth_item(GTK_TOOLBAR(toolbar), 0);
-  if (g_sendbtn != NULL){
-    debug_print("[PLUGIN] toolitem:%p\n", g_sendbtn);
-
-#if 0
-    /**/
-    g_sendbtn_id = g_signal_handler_find(G_OBJECT(g_sendbtn),
-                                         G_SIGNAL_MATCH_ID|G_SIGNAL_MATCH_UNBLOCKED,
-                                         g_signal_id, 0, NULL, NULL, NULL);
-    if (g_sendbtn_id == 0) {
-      debug_print("[PLUGIN] can not find toolitem signal handler id.\n");
-    } else {
-      debug_print("[PLUGIN] toolitem signal handler id is %ld.\n", g_sendbtn_id);
-
-      if (g_hook_id == 0){
-        debug_print("[PLUGIN] add emission hook for signal %d.\n", g_signal_id);
-        g_hook_id = g_signal_add_emission_hook(g_signal_id, 0, button_press_emission_hook, compose, destroy_notify);
-        debug_print("[PLUGIN] emission hook handler %ld.\n", g_hook_id);
-      }
-      /* block sending mail */
-      /*g_signal_handler_block(G_OBJECT(GTK_BIN(g_sendbtn)->child), g_sendbtn_id);*/
-    }
-
-#else
-    g_sendbtn_id = g_signal_handler_find(G_OBJECT(GTK_BIN(g_sendbtn)->child),
-                                         G_SIGNAL_MATCH_ID|G_SIGNAL_MATCH_UNBLOCKED,
-                                         g_signal_id, 0, NULL, NULL, NULL);
-    if (g_sendbtn_id == 0) {
-      debug_print("[PLUGIN] can not find toolitem signal handler id.\n");
-    } else {
-      debug_print("[PLUGIN] toolitem signal handler id is %ld.\n", g_sendbtn_id);
-
-      if (g_hook_id == 0){
-        debug_print("[PLUGIN] add emission hook for signal %d.\n", g_signal_id);
-        g_hook_id = g_signal_add_emission_hook(g_signal_id, 0, button_press_emission_hook, compose, destroy_notify);
-        debug_print("[PLUGIN] emission hook handler %ld.\n", g_hook_id);
-      }
-      /* block sending mail */
-      gboolean bconnected = g_signal_handler_is_connected(G_OBJECT(GTK_BIN(g_sendbtn)->child), g_sendbtn_id);
-      debug_print("[PLUGIN] button handler id connected ? %s.\n", bconnected ? "TRUE" : "FALSE");
-  
-      /* g_signal_handler_block(G_OBJECT(GTK_BIN(g_sendbtn)->child), g_sendbtn_id);*/
-
-      guint blocked = g_signal_handlers_block_matched(G_OBJECT(GTK_BIN(g_sendbtn)->child),
-                                                      G_SIGNAL_MATCH_ID,
-                                                      g_signal_id,
-                                                      0,
-                                                      NULL,
-                                                      NULL,
-                                                      NULL);
-      debug_print("[PLUGIN] number of blocked handler is %d.\n", blocked);
-
-      g_signal_handler_block(G_OBJECT(GTK_BIN(g_sendbtn)->child), g_sendbtn_id);
-      
-    }
-#endif
-  }
 }
 
 void compose_destroy_cb(GObject *obj, gpointer compose)
@@ -355,6 +217,20 @@ void compose_destroy_cb(GObject *obj, gpointer compose)
     g_hook_id = 0;
   }
   
+}
+
+gboolean compose_send_cb(GObject *obj, gpointer compose)
+{
+  debug_print("[PLUGIN] compose_send_cb is called.\n");
+  /* stop furthor event handling. */
+  return TRUE;
+}
+
+gboolean compose_sendl_cb(GObject *obj, gpointer compose)
+{
+  debug_print("[PLUGIN] compose_destroy_cb is called.\n");
+  /* stop furthor event handling. */
+  return TRUE;
 }
 
 gboolean send_button_press(GtkWidget	*widget,
