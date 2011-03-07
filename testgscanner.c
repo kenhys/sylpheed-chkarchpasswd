39f0d18fcbcf1edbaa26cfc258fd29921f4fa12b
HAYASHI Kentaro <kenhys@gmail.com>
2011-03-07 15:57:19 +0900

    remove 0xe308 from string. dirty hack.
    original bzr status:
    revno: 36
    committer: HAYASHI Kentaro <kenhys@gmail.com>
    branch nick: chkarchpasswd
    timestamp: Mon 2011-03-07 15:57:19 +0900

--- testgscanner.c
+++ testgscanner.c
@@ -20,6 +20,7 @@ int main(int argc, char*argv[])
   g_scanner_input_text(gscan, text, strlen(text));
 
   GTokenValue gvalue;
+  int index=0;
   while( g_scanner_eof(gscan) != TRUE){
     GTokenType gtoken = g_scanner_get_next_token (gscan);
     switch (gtoken){
@@ -29,6 +30,15 @@ int main(int argc, char*argv[])
       break;
     case G_TOKEN_IDENTIFIER:
       gvalue = g_scanner_cur_value(gscan);
+      for (index = 0; index<strlen(gvalue.v_identifier);index++){
+        if (index == strlen(gvalue.v_identifier)-1 &&
+            gvalue.v_identifier[index] & 0xe308){
+          g_print("0xe308 found:%d\n", index);
+          gvalue.v_identifier[index]= '\0';
+        }else{
+          g_print("char :%0x08\n", gvalue.v_identifier[index]);
+        }
+      }
       g_print("identifier:%s\n", gvalue.v_identifier);
       break;
     default:
